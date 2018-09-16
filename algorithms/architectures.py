36e1985d189fb2c05bf9cf5d36d3f71414324af9
Yi Liu <yiliu77@berkeley.edu>
2018-09-15 17:37:15 -0700

    Added Convolutional Layer

--- algorithms/architectures.py
+++ algorithms/architectures.py
@@ -40,5 +40,37 @@ def LSTM(self, _input, hparams, name="lstm"):
 
 
 def CNN(self, _input, hparams, name="cnn"):
-	# TODO
-	raise NotImplementedError('AHH')
+	"""
+	Builds a Convolutional NN with a flattened output
+
+	Args:
+		_input: Tensor of shape [None, image_height, image_width, channels]
+		hparams: Dictionary of hyperparameters
+			'feature_maps': List of feature maps for each layer
+			'kernel_sizes': List of kernel sizes for each layer
+			'stride_lengths': List of strides for each layer
+			'padding_types': List of padding for each layer
+			'activations': List of activation functions for each layer
+	@Author: Yi Liu
+	"""
+
+	net = _input
+	feature_maps = hparams['feature_maps']
+	kernel_sizes = hparams['kernel_sizes']
+	stride_lengths = hparams['stride_lengths']
+	padding_types = hparams['padding_types']
+	activations = hparams['activations']
+
+	with tf.variable_scope(name):
+		for i in range(len(activations)):
+			net = tf.layers.conv2d(
+				inputs=net,
+				filters=feature_maps[i],
+				kernel_size=kernel_sizes[i],
+				strides=stride_lengths[i],
+				padding=padding_types[i],
+				activation=activations[i]
+			)
+		# Flatten network
+		flat = tf.contrib.layers.flatten(net)
+	return flat
