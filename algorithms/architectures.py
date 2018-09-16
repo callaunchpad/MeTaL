65f7f0ef12c5f844372a749e32473cca59d04a84
Yi Liu <yiliu77@berkeley.edu>
2018-09-15 17:39:06 -0700

    Added Convolutional Layer

--- algorithms/architectures.py
+++ algorithms/architectures.py
@@ -7,32 +7,32 @@ import tensorflow as tf
 
 
 def FeedForward(self, _input, hparams, name="ffn"):
-		"""
-		Builds a Feed Forward NN with linear output
+	"""
+	Builds a Feed Forward NN with linear output
 
-		Args:
-			_input: Tensor of shape [None, input_size]
-			hparams: Dictionary of hyperparameters
-				'output_size': Dimensionality of output
-				'hidden_sizes': List of hidden layer sizes
-				'activations': List of activation functions for each layer
-		@Author: Arsh Zahed
-		"""
+	Args:
+		_input: Tensor of shape [None, input_size]
+		hparams: Dictionary of hyperparameters
+			'output_size': Dimensionality of output
+			'hidden_sizes': List of hidden layer sizes
+			'activations': List of activation functions for each layer
+	@Author: Arsh Zahed
+	"""
 
-		# Placeholder for input
+	# Placeholder for input
 
-		# We iteratively nest the layers
-		net = _input
-		hidden_sizes = hparams['hidden_sizes']
-		activations = hparams['activations']
-		with tf.variable_scope('dense_layers'):
-			for i in range(len(activations)):
-				net = tf.layers.dense(net, hidden_sizes[i], activations[i])
-			# Call our prediction/policy y_hat. 
-			# Linear activation allows for logits
-			y_hat = tf.layers.dense(net, hparams['output_size'])
+	# We iteratively nest the layers
+	net = _input
+	hidden_sizes = hparams['hidden_sizes']
+	activations = hparams['activations']
+	with tf.variable_scope('dense_layers'):
+		for i in range(len(activations)):
+			net = tf.layers.dense(net, hidden_sizes[i], activations[i])
+		# Call our prediction/policy y_hat.
+		# Linear activation allows for logits
+		y_hat = tf.layers.dense(net, hparams['output_size'])
 
-		return y_hat
+	return y_hat
 
 def LSTM(self, _input, hparams, name="lstm"):
 	# TODO
