4dd56ca7a4fdf7d2b99303061636d8603f655f47
Hank O'Brien <hank.obrien900@icloud.com>
2018-09-29 16:43:36 -0700

    documentation, formatting

--- algorithms/natural_grad_update.py
+++ algorithms/natural_grad_update.py
@@ -11,33 +11,33 @@ def conj_grad_func(A, b):
 	assert(np.shape(A)[0] == np.shape(A)[1] and np.shape(b)[0] == np.shape(A)[0] and np.shape(b)[1] == 1)
 	return np.array([scipy.sparse.linalg.cg(A,b)[0]]).T
 
-# there may be some issues with distributed training when using this function
+def conj_grad_wrapper(A,b):
+	"""
+	wrapper for tf.py_func so that we can pass dynamic A and b values
+	@Author Hank O'Brien
+	"""
+	# there may be some issues with distributed training when using this function
+	return tf.py_func(conj_grad_func, [A, b], tf.float32, stateful=False, name='conj_grad_func')
+
 A = tf.placeholder(dtype=tf.float32, shape=[None, None])
 b = tf.placeholder(dtype=tf.float32, shape=[None, 1])
 
-# conj_grad_op = tf.py_func(conj_grad_func, [A, b], tf.float32, stateful=False, name='conj_grad_func')
-
-def conj_grad_wrapper(A,b):
-	return tf.py_func(conj_grad_func, [A, b], tf.float32, stateful=False, name='conj_grad_func')
-
 if __name__ == '__main__':
-
-	# A_placeholder = tf.placeholder(tf.float32, shape=[3,3])
-	# b_placeholder = tf.placeholder(tf.float32, shape=[3,1])
-
 	output = conj_grad_wrapper(A, b)
 
-	# with tf.Graph().as_default():
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
+		#an arbitrary matrix
 		A_input = np.array([[1,0,2],[3,4,5],[6,9,1]])
+		#make the matrix positive definite
 		A_input = A_input.T @ A_input
+		#arbitrary output
 		b_input = np.array([[1,2,3]]).T
-
+		#computed output
 		x_out = sess.run([output], feed_dict={A: A_input, b: b_input})
 
-		
+		#test if computed output is the same as 'correct' output (found through other technique)
 		if(np.allclose(x_out[0], np.linalg.solve(A_input,b_input), rtol=0.0001, atol=0.0001, equal_nan=False)):
 			print("passed test")
 		else:
-			raise AssertionError('output was not expected')
+			raise AssertionError('output was different than expected')
