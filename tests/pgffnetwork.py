ee7efc9800f53d2027111cc5eacffaad61cfcf68
jhejna <joey.hejna@gmail.com>
2018-09-28 20:02:35 -0700

    implemented support for multiple environments

--- tests/pgffnetwork.py
+++ tests/pgffnetwork.py
@@ -21,14 +21,16 @@ n_max_iter = 150000
 n_games = 1500
 # number of episodes to run for each game
 n_episodes = 100
+# number of environments to run
+n_environments = 5
+
 discount_rate = 0.96
 
-env = gym.make('CartPole-v0')
-env._max_episode_steps = n_max_iter
 # environment observation size
 env_obs_n = 4
 # environment action size
 env_act_n = 2
+
 
 ff_hparams = {
     'hidden_sizes': [30, 30],
@@ -41,41 +43,54 @@ ff_hparams = {
 
 learning_rate = 0.004
 sess = tf.InteractiveSession()
-agent = PGFFNetwork(sess, env_obs_n, env_act_n, ff_hparams, learning_rate, n_episodes)
+agent = PGFFNetwork(sess, env_obs_n, env_act_n, ff_hparams, learning_rate, n_environments, 0)
 tf.global_variables_initializer().run()
+
+
+envs = [gym.make("CartPole-v0") for _ in range(n_environments)]
+for env in envs:
+    env._max_episode_steps = n_max_iter
 
 for game in range(n_games):
     # store states, actions, and rewards
-    states = []
-    actions = []
-    rewards = []
+    observations = np.array([env.reset() for env in envs])
+    states = [[]] * n_environments
+    actions = [[]] * n_environments
+    rewards = [[]] * n_environments
+    dones = [None] * n_environments
     discounted_rewards = []
-    for episode in range(agent.n_episodes):
-        states.append([])
-        actions.append([])
-        rewards.append([])
-        discounted_rewards.append([])
-        obs = env.reset()
-        for _ in range(n_max_iter):
-            action_dist = agent.action_dist(obs[np.newaxis, :])
-            action = np.random.choice(np.arange(env_act_n), p=np.squeeze(action_dist))
+
+    for _ in range(n_max_iter):
+        action_dist = agent.action_dist(observations[:, np.newaxis, :])
+       
+        for env_index, env in enumerate(envs):
+            action = np.random.choice(np.arange(env_act_n), p=np.squeeze(action_dist[env_index]))
             obs, reward, done, info = env.step(action)
+            obs = obs if not done else env.reset()
+            states[env_index].append(obs)
+            actions[env_index].append(action)
+            rewards[env_index].append(reward)
+            dones[env_index] = done
+        
+        # This probably needs to change?
+        if any(dones):
+            break
 
-            states[-1].append(obs)
-            actions[-1].append(action)
-            rewards[-1].append(reward)
-            if done:
-                break
-
-        # discount rewards
+    agregated_rewards = []
+    # discount rewards
+    for env_index, env in enumerate(envs):
+        discounted_rewards = []
         accumulated_reward = 0
-        for step in reversed(range(len(rewards))):
-            accumulated_reward = rewards[step] + accumulated_reward * discount_rate
-            discounted_rewards[-1].insert(0, accumulated_reward)
-    # normalize discounted rewards
-    discounted_rewards -= np.mean(discounted_rewards)
-    discounted_rewards /= np.std(discounted_rewards)
+        for step in reversed(range(len(rewards[env_index]))):
+            accumulated_reward = rewards[env_index][step] + accumulated_reward * discount_rate
+            discounted_rewards.insert(0, accumulated_reward)
+        # normalize discounted rewards
+        discounted_rewards -= np.mean(discounted_rewards)
+        discounted_rewards /= np.std(discounted_rewards)
+        agregated_rewards.append(discounted_rewards)
 
     # train agent
-    error = agent.train(states, actions, discounted_rewards)
-    print("Game: {}, Error: {}, Game Length: {}, Total Reward: {}".format(game, error, len(actions), sum(rewards)))
+    error = agent.train(states, actions, agregated_rewards)
+    avg_error = np.mean(error)
+    print("Game: {}, Average Error: {}".format(game, avg_error))
+    #print("Game: {}, Average Error: {}, Game Length: {}, Total Reward: {}".format(game, avg_error, len(actions), sum(rewards)))
