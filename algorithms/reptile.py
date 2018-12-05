2676a3b35707b6e2287cd2cdcd19c3644a4713c2
jhejna <joey.hejna@gmail.com>
2018-12-04 17:33:00 -0800

    redo reptile args, implement saving, single grad step eval

--- algorithms/reptile.py
+++ algorithms/reptile.py
@@ -11,17 +11,25 @@ Classes and functions for metalearning with repile and sampling from tasks.
 
 
 class Reptile:
-	def __init__(self, session, model, tasks, variables=None, load_buffer_from_pickle=False, save_buffer_to_pickle=False, load_buffer_pickle_path=None, save_buffer_pickle_path=None):
+	def __init__(self, session, model, tasks, FLAGS, variables=None, load_buffer_from_pickle=False, save_buffer_to_pickle=False, load_buffer_pickle_path=None, save_buffer_pickle_path=None):
 		self.sess = session
 		self.model = model
 		self.tasks = tasks
 		self._model_state = VariableState(self.sess, variables or tf.trainable_variables())
-		self.discount_rate = 0.95
+
 		self.reward_threshold = float('inf') # TODO
 		self.replay_buffer = {}
 		self.save_buffer_to_pickle = save_buffer_to_pickle
 		self.load_buffer_pickle_path = load_buffer_pickle_path
 		self.save_buffer_pickle_path = save_buffer_pickle_path
+
+		self.n_max_iter = FLAGS.max_episodes
+		self.discount_rate = FLAGS.discount
+		self.steps = FLAGS.steps
+		self.single_task_steps = FLAGS.single_task_steps
+		self.meta_step_size = FLAGS.meta_step_size
+		self.env_act_n = FLAGS.env_act_n
+
 		if load_buffer_from_pickle:
 			try:
 				with open(load_buffer_pickle_path, 'rb') as buffer_file:
@@ -31,68 +39,68 @@ class Reptile:
 		for task in tasks:
 			self.replay_buffer[task] = []
 
-	def train(self):
-		steps = 200
-		single_task_steps = 5
-		meta_step_size = 0.1
 
-		for step in range(steps):
+		# Save model to be loaded later
+		self.saver = tf.train.Saver(max_to_keep=2)
+
+	def train(self):
+		save_step = 20
+		for step in range(self.steps):
 			task_num, task = random.choice(list(enumerate(self.tasks)))
 			#print(task, task_num)
 
 			old_vars = self._model_state.export_variables()
-			for i in range(single_task_steps):
+			for i in range(self.single_task_steps):
 				(states, actions, rewards, avg_len) = self.sample_from_task(self.model, task)
 				feed_dict = {self.model.s: states, self.model.a: actions, self.model.r: rewards}
 				self.sess.run(self.model.train_op, feed_dict=feed_dict)
 
 				# compute discounted accumulated reward
 				accumulated_reward = 0
-				for step in reversed(range(len(rewards))):
-					accumulated_reward = rewards[step] + accumulated_reward * self.discount_rate
+				for inner_step in reversed(range(len(rewards))):
+					accumulated_reward = rewards[inner_step] + accumulated_reward * self.discount_rate
 
 				# Add rollouts with good performance to replay buffer for task for importance sampling
 				if accumulated_reward > self.reward_threshold:
 					self.replay_buffer[task].append((states, actions, rewards))
-				print("Update: {}, Task: {}, Game Length: {}".format(i+step*single_task_steps,task_num, avg_len))
+				print("Update: {}, Task: {}, Game Length: {}".format(i+step*self.single_task_steps,task_num, avg_len))
 
 			new_vars = self._model_state.export_variables()
 
-			self._model_state.import_variables(interpolate_vars(old_vars, new_vars, meta_step_size))
+			if step % save_step == 0:
+				print("SAVING MODEL", step)
+				self.saver.save(self.sess, 'reptile_cart_model/meta-agent', global_step=step)
+
+			self._model_state.import_variables(interpolate_vars(old_vars, new_vars, self.meta_step_size))
 
 		if self.save_buffer_to_pickle:
 			with open(self.save_buffer_pickle_path, 'wb') as buffer_file:
 				pickle.dump(self.replay_buffer, buffer_file)
 
 
+	def load(self):
+		self.saver.restore(self.sess, self.saver.last_checkpoints[-1])
 	#Sample a set of states, actions, and rewards from the model for a given task
 	"""
 	task: A game meta-learning will be used on. Must be openai environment. 
 	model: tf model takes in state, outputs action distribution. Needs s and outputs. 
 	"""
 	def sample_from_task(self, model, task):
-		#max steps in environment
-		n_max_iter = 1500
-		# environment observation size
-		env_obs_n = 4
-		# environment action size
-		env_act_n = 2
-		total_samples = 200
-
-		
+	
 		# store states, actions, and rewards
 		states = []
 		actions = []
 		rewards = []
+		total_samples = 200
 
 		games = 0
 		while(len(states) < total_samples):
 			games += 1
 			obs = task.reset()
-			for _ in range(n_max_iter):
+			for _ in range(self.n_max_iter):
 
 				action_dist = self.sess.run(model.outputs, feed_dict={model.s: obs[np.newaxis, :]})
-				action = np.random.choice(np.arange(env_act_n), p=np.squeeze(action_dist))
+				action = np.random.choice(np.arange(self.env_act_n), p=np.squeeze(action_dist))
 				new_obs, reward, done, info = task.step(action)
 
 				states.append(obs)
--- algorithms/reptile_train.py
+++ algorithms/reptile_train.py
@@ -22,93 +22,97 @@ sys.path.insert(0, os.path.dirname(dir_path))
 from algorithms.policygrad import PGFFNetwork
 
 flags.DEFINE_string('save_dir', '/tmp/train_log', 'Directory to checkpoint to.')
-flags.DEFINE_integer('max_episodes', 150000, 'Maximum number of episodes.')
-flags.DEFINE_integer('num_games', 1500, 'Number of games played')
+flags.DEFINE_integer('max_episodes', 1500, 'Maximum number of episodes.')
 flags.DEFINE_float('learning_rate', .01, 'Learning rate.')
 flags.DEFINE_float('discount', .95, 'MDP discount rate.')
+flags.DEFINE_integer('env_act_n', 2, 'Number of actions in environment')
+flags.DEFINE_integer('env_obs_n', 4, 'Number of observations in environment')
+flags.DEFINE_integer('single_task_steps', 5, 'Number of times to repeat a given task in reptile training')
+flags.DEFINE_integer('steps', 21, 'Number of times to sample tasks. Total steps is steps * single_task_steps')
+flags.DEFINE_float('meta_step_size', 0.1, 'Weight to give to each reptile update')
 
 FLAGS = flags.FLAGS
 
 
 def main(argv):
-    del argv  # Unused
+	del argv  # Unused
 
-    n_max_iter = FLAGS.max_episodes
-    n_games = FLAGS.num_games
-    discount_rate = FLAGS.discount
-    learning_rate = FLAGS.learning_rate
+	n_max_iter = FLAGS.max_episodes
+	discount_rate = FLAGS.discount
+	learning_rate = FLAGS.learning_rate
 
-    #Create a set of tasks for meta-learning
-    tasks = []
-    for i in range(20):
-        task = gym.make('CartPole-v0')
-        task._max_episode_steps = n_max_iter
-        task.unwrapped.gravity += random.random()*2-1
-        task.unwrapped.length += 0.2*random.random()-0.1
-        tasks.append(task)
+	#Create a set of tasks for meta-learning
+	tasks = []
+	for i in range(20):
+		task = gym.make('CartPole-v0')
+		task._max_episode_steps = n_max_iter
+		task.unwrapped.gravity += random.random()*2-1
+		task.unwrapped.length += 0.2*random.random()-0.1
+		tasks.append(task)
 
-    # Setup Gym Environment
-    env = gym.make('CartPole-v0')
-    env._max_episode_steps = n_max_iter
+	# Setup Gym Environment
+	env = gym.make('CartPole-v0')
+	env._max_episode_steps = n_max_iter
 
-    # environment observation size
-    env_obs_n = 4
-    # environment action size
-    env_act_n = 2
+	# environment observation size
+	env_obs_n = 4
+	# environment action size
+	env_act_n = 2
 
-    #Create agent
-    ff_hparams = {
-        'hidden_sizes': [30, 30],
-        'activations': [tf.nn.leaky_relu, tf.nn.leaky_relu],
-        'output_size': env_act_n,
-        'kernel_initializers': [tf.contrib.layers.xavier_initializer(),
-                                tf.contrib.layers.xavier_initializer(),
-                                tf.contrib.layers.xavier_initializer()]
-    }
+	#Create agent
+	ff_hparams = {
+		'hidden_sizes': [30, 30],
+		'activations': [tf.nn.leaky_relu, tf.nn.leaky_relu],
+		'output_size': env_act_n,
+		'kernel_initializers': [tf.contrib.layers.xavier_initializer(),
+								tf.contrib.layers.xavier_initializer(),
+								tf.contrib.layers.xavier_initializer()]
+	}
 
-    agent = PGFFNetwork(env_obs_n, env_act_n, ff_hparams, learning_rate)
+	agent = PGFFNetwork(env_obs_n, env_act_n, ff_hparams, learning_rate)
 
+	with tf.Session() as sess:
+		tf.global_variables_initializer().run()
+		saver = tf.train.Saver()
+		# Do meta learning
+		reptile = Reptile(sess, agent, tasks, FLAGS)
+		reptile.train()
 
-    with tf.Session() as sess:
-        tf.global_variables_initializer().run()
+		# Test meta learning on original game
+		for task in tasks:
+			reptile.load()
 
-        #Do meta learning
-        reptile = Reptile(sess, agent, tasks)
-        reptile.train()
+			obs = task.reset()
+			# store states, actions, and rewards
+			states = []
+			actions = []
+			rewards = []
+			for _ in range(n_max_iter):
+				action_dist = agent.action_dist(obs[np.newaxis, :], sess)
+				action = np.random.choice(np.arange(env_act_n), p=np.squeeze(action_dist))
+				new_obs, reward, done, info = task.step(action)
 
-        #Test meta learning on original game
-        for game in range(n_games):
-            obs = env.reset()
-            # store states, actions, and rewards
-            states = []
-            actions = []
-            rewards = []
-            for _ in range(n_max_iter):
-                action_dist = agent.action_dist(obs[np.newaxis, :], sess)
-                action = np.random.choice(np.arange(env_act_n), p=np.squeeze(action_dist))
-                new_obs, reward, done, info = env.step(action)
+				states.append(obs)
+				actions.append(action)
+				rewards.append(reward)
+				obs = new_obs
+				if done:
+					break
 
-                states.append(obs)
-                actions.append(action)
-                rewards.append(reward)
-                obs = new_obs
-                if done:
-                    break
+			# discount rewards
+			discounted_rewards = []
+			accumulated_reward = 0
+			for step in reversed(range(len(rewards))):
+				accumulated_reward = rewards[step] + accumulated_reward * discount_rate
+				discounted_rewards.insert(0, accumulated_reward)
+			# normalize discounted rewards
+			discounted_rewards -= np.mean(discounted_rewards)
+			discounted_rewards /= np.std(discounted_rewards)
 
-            # discount rewards
-            discounted_rewards = []
-            accumulated_reward = 0
-            for step in reversed(range(len(rewards))):
-                accumulated_reward = rewards[step] + accumulated_reward * discount_rate
-                discounted_rewards.insert(0, accumulated_reward)
-            # normalize discounted rewards
-            discounted_rewards -= np.mean(discounted_rewards)
-            discounted_rewards /= np.std(discounted_rewards)
-
-            # train agent
-            error = agent.train(states, actions, discounted_rewards, sess)
-            print("Game: {}, Error: {}, Game Length: {}, Total Reward: {}".format(game, error, len(actions), sum(rewards)))
+			# train agent
+			error = agent.train(states, actions, discounted_rewards, sess)
+			print("Gravity: {}, Length: {}, Error: {}, Game Length: {}, Total Reward: {}".format(task.unwrapped.gravity, task.unwrapped.length, error, len(actions), sum(rewards)))
 
 
 if __name__ == "__main__":
-    app.run(main)
+	app.run(main)
